0e16912f5be3661b6b2c1ff7ce2b50f45f06e7d3
shahriyardx <mdshahriyaralam552@gmail.com>
2023-07-19 02:19:55 +0600

    use env from env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,4 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const MONGO_URL = process.env.MONGO_URL as string
+export const PORT = Number(process.env.PORT || 5000)
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import app from "./app"
-import dotenv from "dotenv"
 import { Request, Response } from "express"
 import mongoose from "mongoose"
-import { MONGO_URL } from "./env"
-
-dotenv.config()
+import { MONGO_URL, PORT } from "./env"
 
 app.get("/", (_req: Request, res: Response) => {
   return res.json({ message: "backend service is live" })
 })
-
-const PORT = process.env.PORT || 5000
 
 const main = async () => {
   try {
