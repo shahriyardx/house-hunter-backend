b459ae65d60544b007606e5ec3138224780b94fe
shahriyardx <mdshahriyaralam552@gmail.com>
2023-07-19 23:16:04 +0600

    fix pagination

--- src/routers/public.ts
+++ src/routers/public.ts
@@ -6,12 +6,13 @@ const publicRouter = Router()
 publicRouter.get("/houses", async (req, res) => {
   const query = req.query
   const search = query.query || ""
-  console.log(search)
-
+  const page = Number(query.page) || 1
   const totalHouses = await House.countDocuments({ name: { $regex: search } })
   const houses = await House.find({
     name: { $regex: search },
-  }).limit(10)
+  })
+    .skip((page - 1) * 10)
+    .limit(10)
 
   res.json({ status: "success", message: "", data: { houses, totalHouses } })
 })
